5121d8d145cd45d333f077bb7533e829b933fed2
Yashneel chauhan <yashneelchauhan69@gmail.com>
2025-01-28 20:56:05 +0530

    Add files via upload

---
+++ Calculator .py
@@ -0,0 +1,15 @@
+num1 = int(input("Enter your first number : "))
+num2 = int(input("Enter your second number : "))
+symbol = input("What you want to do?\n")
+
+if ( symbol == "+" ):
+    print( num1 + num2)
+
+elif ( symbol == "-" ):
+    print( num1 - num2)
+
+elif ( symbol == "*" ):
+    print( num1 * num2)
+
+elif ( symbol == "/" ):
+    print( num1 / num2)
